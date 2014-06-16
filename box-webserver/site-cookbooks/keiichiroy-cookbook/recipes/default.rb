3a500950906809a897c91504d986f9656bdb7bac
keiichiroy <k.yanmar@gmail.com>
2014-06-17 01:42:03 +0900

    CentOSのバージョンを5.10->6.5に変更,recipeのyum_packageを複数可能なように準備

--- box-webserver/site-cookbooks/keiichiroy-cookbook/recipes/default.rb
+++ box-webserver/site-cookbooks/keiichiroy-cookbook/recipes/default.rb
@@ -1,7 +1,12 @@
-yum_package "httpd" do
-  action :install
+# install some yum packages
+install_packages = ["httpd"]
+install_packages.each do |pkg|
+  yum_package "#{pkg}" do
+    action :install
+  end
 end
 
+# register/start httpd service
 service "httpd" do
   supports :status => true, :restart => true, :reload => true
   action [:enable, :start]
